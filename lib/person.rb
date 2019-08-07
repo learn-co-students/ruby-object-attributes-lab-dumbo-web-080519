f1bd7fa6ea067dee1e95b54d61639b6cde8c9f19
Brian Wong <bwong92@gmail.com>
2019-08-07 02:44:45 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,18 @@
+class Person
+  #getter
+  def job
+    @job
+  end
+  #setter
+  def job=(job_title)
+    @job=job_title
+  end
+  #getter
+  def name
+    @name
+  end
+
+  def name=(person_name)
+    @name = person_name
+  end
+end
