394dd9e8fdb3fd284fa418b19e6669b7c03e1779
Brandon James <brandonj2858@gmail.com>
2019-08-13 09:40:04 -0400

    done

---
+++ lib/dog.rb
@@ -0,0 +1,12 @@
+class Dog
+
+attr_accessor :name, :breed
+
+
+  
+
+  def name=(new_name)
+    @name = new_name
+  end
+
+end
---
+++ lib/person.rb
@@ -0,0 +1,15 @@
+class Person
+  attr_accessor :name, :job
+
+  def name=(new_name)
+    @name = new_name
+  end
+
+    def job=(new_job)
+      @job = new_job
+    end
+
+
+
+
+end
